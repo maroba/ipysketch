d6c0059fac1986375eace584cb0023e09a312203
Matthias Baer <matthias.baer@mpl.mpg.de>
2021-08-30 13:38:32 +0200

    Reset

--- ipysketch/kivyapp.py
+++ ipysketch/kivyapp.py
@@ -7,7 +7,10 @@ from kivy.graphics.vertex_instructions import Line, Ellipse, Rectangle
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.slider import Slider
+from kivy.uix.label import Label
+from kivy.uix.dropdown import DropDown
 
 from kivy.graphics.instructions import InstructionGroup
 
